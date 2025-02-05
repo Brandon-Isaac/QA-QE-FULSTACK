77a7870b3a90ed2319c0e8c356fc2dfa6c52ae1d
Brandon-Isaac <isaac.datch22@students.dkut.ac.ke>
2025-02-05 21:47:30 +0300

    day 3 1st commit

---
+++ JS/1.basics/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk"
+console.log(chalk.bgRed("Hello world"));
+const text="🌍🌏";
+const blueWorld=chalk.bgBlue(text);
+console.log(blueWorld);
---
+++ JS/2.variables/variable.js
@@ -0,0 +1,35 @@
+let age = 25;
+const schoolName = "Greenwood High";
+let studentsList = [];
+console.log(typeof 99);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof 3089968859905885889n);
+let data = ["Kenya", 34, false, { country: "USA" }, null];
+console.log(typeof data);
+
+const person = {
+  name: "Datch",
+  age: 21,
+  city: "Kisumu",
+};
+person.email = "isaacdutch472@gmail.com";
+person.password = "password";
+
+const fruits = ["mango", "apple", "oranges"];
+let favFruit = fruits[1];
+console.log(favFruit);
+
+console.log("5" - 2);
+console.log(5 + "2");
+
+const text = "100";
+let num = parseInt(text);
+console.log(num + " is a " + typeof num);
+
+const num2 = 50;
+let text2 = num2.toString();
+console.log(text2 + " is a " + typeof text2);
+
+console.log(5 + true);
+console.log(person);
